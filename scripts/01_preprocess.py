ad1ba14889cb6213304867d07be8012a0ad092b4
Sebastian Gelves <juan_sebas_gelvez@hotmail.com>
2025-06-15 16:19:50 -0500

    data encoded done

--- scripts/01_preprocess.py
+++ scripts/01_preprocess.py
@@ -7,8 +7,8 @@ from sentiment_classification.preprocessing.encoding import encode_labels_train,
 
 
 # Leemos datos
-df_train = load_data('../data/raw/Corona_NLP_train.csv')
-df_test = load_data('../data/raw/Corona_NLP_test.csv')
+df_train = load_data('data/raw/Corona_NLP_train.csv')
+df_test = load_data('data/raw/Corona_NLP_test.csv')
 
 
 ### Data Cleaning ###
@@ -22,8 +22,8 @@ df_train_clean['OriginalTweet'] = df_train_clean['OriginalTweet'].apply(clean_te
 df_test_clean['OriginalTweet'] = df_test_clean['OriginalTweet'].apply(clean_text)
 
 # Guardamos los datos limpios
-df_train_clean.to_csv('../data/clean/train_clean.csv')
-df_test_clean.to_csv('../data/clean/test_clean.csv')
+df_train_clean.to_csv('data/clean/train_clean.csv')
+df_test_clean.to_csv('data/clean/test_clean.csv')
 
 # Mensaje informando que se han guardado los datos limpios
 print('\n #### Los datos limpios han sido guardados en la carpeta /data/clean #### \n')
@@ -34,8 +34,18 @@ print('\n #### Los datos limpios han sido guardados en la carpeta /data/clean ##
 # Hacemos la codificación de Sentiment con un ordinal encoder
 # Codificamos train
 df_train_processed = df_train_clean.copy()
-df_train_processed["Sentiment"], encoder = encode_labels_train(df_train_processed["Sentiment"], save_path='../models/ordinal_encoder.pkl')
+df_train_processed["Sentiment"], encoder = encode_labels_train(df_train_processed["Sentiment"], save_path='models/ordinal_encoder.pkl')
 
 # Codificamos  test
 df_test_processed = df_test_clean.copy()
-df_test_processed["Sentiment"] = encode_labels_test(df_test_processed["Sentiment"], encoder_path="../models/ordinal_encoder.pkl")
+df_test_processed["Sentiment"] = encode_labels_test(df_test_processed["Sentiment"], encoder_path="models/ordinal_encoder.pkl")
+
+# Mensaje informando que se han codificado los datos y guardado el encoder
+print('\n #### Los datos han sido codificados y el encoder se ha guardado en la carpeta /models #### \n')
+
+# Guardamos datos processed
+df_train_processed.to_csv('data/processed/train_encoded.csv')
+df_test_processed.to_csv('data/processed/test_encoded.csv')
+
+# Mensaje informando que se han guardado los datos encoded
+print('\n #### Los datos con la columna Sentiment codificada han sido guardados en la carpeta /data/processed #### \n')
